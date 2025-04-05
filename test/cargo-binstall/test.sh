b1869e6f879cb199ab6f1b5af583cbc18c55054f
Marcus Johnson <mejohnson93@gmail.com>
2025-04-05 02:59:59 +0000

    Fix cargo-binstall

--- test/cargo-binstall/test.sh
+++ test/cargo-binstall/test.sh
@@ -8,9 +8,9 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "installed" cargo install --list | grep "cargo-binstall"
 
-if !cargo binstall  --locked mprocs  -y > /dev/null; then
+if ! cargo binstall  --locked mprocs  -y > /dev/null; then
     cargo install  --locked mprocs 
-}
+fi
 
 check "installed" cargo install --list  | grep "mprocs"
 
